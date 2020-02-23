d9580d740cd4b93c6473720fda3d46f3cada3301
nyomo <nyomoko@gmail.com>
2020-02-23 21:06:41 +0900

    チャンネルリストが取得済みなら再取得しない

--- SlackAPI.js
+++ SlackAPI.js
@@ -25,7 +25,7 @@ class SlackAPI {
     */
     var list = JSON.parse(this.post("channels.list",options));
     var result = Array();
-    if(list["ok"] == true){
+    if(list["ok"] == true && this.channels_list.length == 0){
       this.channels = result.concat(this.channels,list.channels)
       while(Object.keys(list).indexOf('response_metadata') !== -1 && Object.keys(list.response_metadata).indexOf('next_cursor') !== -1 && list.response_metadata.next_cursor.length > 1){
         list = JSON.parse(this.post("channels.list",{cursor:list["response_metadata"]["next_cursor"]}));
