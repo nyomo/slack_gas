d3a0f7e1064199e8319e37ea4703623771c54663
nyomo <nyomoko@gmail.com>
2020-03-15 00:24:14 +0900

    読みにくかったので改行した

--- SlackAPI.js
+++ SlackAPI.js
@@ -28,7 +28,10 @@ class SlackAPI {
     var result = Array();
     if(list["ok"] == true && this.channels_list.length == 0){
       this.channels = result.concat(this.channels,list.channels)
-      while(Object.keys(list).indexOf('response_metadata') !== -1 && Object.keys(list.response_metadata).indexOf('next_cursor') !== -1 && list.response_metadata.next_cursor.length > 1){
+      while(Object.keys(list).indexOf('response_metadata') !== -1
+            && Object.keys(list.response_metadata).indexOf('next_cursor') !== -1
+            && list.response_metadata.next_cursor.length > 1)
+      {
         options['cursor'] = list["response_metadata"]["next_cursor"];
         list = JSON.parse(this.post("channels.list",options));
         this.channels = result.concat(this.channels,list["channels"])
